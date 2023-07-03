898d93be1155472ce21d437a3270c881b459c552
matsudamper <let@matsudamper.net>
2023-07-04 03:13:12 +0900

    コード生成ライブラリが正式配布されていないのでリポジトリに含む

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,9 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        mavenLocal()
+        mavenLocal {
+            setUrl(".m2/repository")
+        }
     }
 
     plugins {
