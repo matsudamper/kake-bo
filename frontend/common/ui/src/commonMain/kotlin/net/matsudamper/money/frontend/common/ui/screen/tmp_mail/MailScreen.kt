2c1b1cb4fcdaad716aa280a2f4e7d14fcf10016a
matsudamper <let@matsudamper.net>
2023-07-23 03:21:19 +0900

    fix ui

--- frontend/common/ui/src/commonMain/kotlin/net/matsudamper/money/frontend/common/ui/screen/tmp_mail/MailScreen.kt
+++ frontend/common/ui/src/commonMain/kotlin/net/matsudamper/money/frontend/common/ui/screen/tmp_mail/MailScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -174,44 +175,26 @@ public fun MailContent(
             modifier = Modifier.padding(12.dp),
         ) {
             Column(modifier = Modifier.weight(1f)) {
-                Row {
-                    Text(
-                        modifier = Modifier,
-                        text = "From: ",
-                        fontFamily = rememberCustomFontFamily(),
-                    )
-                    Text(
-                        modifier = Modifier,
-                        text = uiState.from,
-                        fontFamily = rememberCustomFontFamily(),
-                    )
-                }
-                Row {
-                    Text(
-                        modifier = Modifier,
-                        text = "Sender: ",
-                        fontFamily = rememberCustomFontFamily(),
-                    )
-                    Text(
-                        modifier = Modifier,
-                        text = uiState.sender.orEmpty(),
-                        fontFamily = rememberCustomFontFamily(),
-                    )
-                }
-                Row {
-                    Text(
-                        modifier = Modifier,
-                        text = "Subject: ",
-                        fontFamily = rememberCustomFontFamily(),
-                    )
-                    Text(
-                        modifier = Modifier,
-                        text = uiState.subject,
-                        fontFamily = rememberCustomFontFamily(),
-                        maxLines = 1,
-                    )
-                }
+                Text(
+                    modifier = Modifier,
+                    text = "From: ${uiState.from}",
+                    fontFamily = rememberCustomFontFamily(),
+                    maxLines = 2,
+                )
+                Text(
+                    modifier = Modifier,
+                    text = "Sender: ${uiState.sender.orEmpty()}",
+                    fontFamily = rememberCustomFontFamily(),
+                    maxLines = 2,
+                )
+                Text(
+                    modifier = Modifier,
+                    text = "Subject: ${uiState.subject}",
+                    fontFamily = rememberCustomFontFamily(),
+                    maxLines = 2,
+                )
             }
+            Spacer(Modifier.width(8.dp))
             Column(
                 horizontalAlignment = Alignment.End,
             ) {
