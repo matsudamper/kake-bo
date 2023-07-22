95cf1763cbb69772553d21b51640fd42029462c0
matsudamper <let@matsudamper.net>
2023-07-23 02:32:36 +0900

    add linux docker build command

---
+++ dockerBuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PATH=$PATH:/home/matsudamper/.jdks/temurin-17.0.8/bin
+
+./gradlew :backend:assemble
+./gradlew :frontend:jsApp:jsBrowserProductionWebpack
+
+docker build -t ghcr.io/matsudamper/kake-bo:latest .
+
+docker login ghcr.io -u matsudamper -p $CR_PAT
+docker push ghcr.io/matsudamper/kake-bo:latest
