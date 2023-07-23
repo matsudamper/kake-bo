a99f7b29f9d08a5059ad1b8acdc9007f7e765552
matsudamper <let@matsudamper.net>
2023-07-24 00:54:43 +0900

    fix index out of bound

--- backend/mail/src/jvmMain/kotlin/net/matsudamper/money/backend/mail/MailRepository.kt
+++ backend/mail/src/jvmMain/kotlin/net/matsudamper/money/backend/mail/MailRepository.kt
@@ -41,7 +41,7 @@ class MailRepository(
             imap4.getFolder("INBOX").let { folder ->
                 folder.open(Folder.READ_ONLY)
                 folder.use {
-                    folder.getMessages(offset, offset + size)
+                    folder.getMessages(offset + 1, offset + size + 1)
                         .map { it as IMAPMessage }
                         .map { message ->
                             val contents = when (val content = message.dataHandler.content) {
