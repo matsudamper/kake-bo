ce5a843b4eaa48ca5dfa9143b66af4ec142d2c6d
matsudamper <let@matsudamper.net>
2023-07-29 00:02:41 +0900

    format

--- frontend/common/viewmodel/src/commonMain/kotlin/net/matsudamper/money/frontend/common/viewmodel/LoginCheckUseCase.kt
+++ frontend/common/viewmodel/src/commonMain/kotlin/net/matsudamper/money/frontend/common/viewmodel/LoginCheckUseCase.kt
@@ -1,23 +1,12 @@
 package net.matsudamper.money.frontend.common.viewmodel
 
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import MailScreenUiState
-import net.matsudamper.money.frontend.common.base.ImmutableList.Companion.toImmutableList
-import net.matsudamper.money.frontend.graphql.GraphqlUserLoginQuery
 import net.matsudamper.money.frontend.common.base.Screen
 import net.matsudamper.money.frontend.common.base.ScreenNavController
-import net.matsudamper.money.frontend.common.base.immutableListOf
 import net.matsudamper.money.frontend.common.viewmodel.lib.EventSender
 import net.matsudamper.money.frontend.common.viewmodel.root.GlobalEvent
-import net.matsudamper.money.frontend.graphql.GetMailQuery
-import net.matsudamper.money.frontend.graphql.MailImportScreenGraphqlApi
+import net.matsudamper.money.frontend.graphql.GraphqlUserLoginQuery
 
 public class LoginCheckUseCase(
     private val ioDispatcher: CoroutineDispatcher,
