b4ea6459d6468a1f274ce465d474eeae639ef4b5
matsudamper <let@matsudamper.net>
2023-07-29 04:46:00 +0900

    Multiplatformで同じアノテーションを要求されるので付ける

--- frontend/common/base/src/commonMain/kotlin/net/matsudamper/money/frontend/common/base/Font.kt
+++ frontend/common/base/src/commonMain/kotlin/net/matsudamper/money/frontend/common/base/Font.kt
@@ -1,6 +1,7 @@
 package net.matsudamper.money.frontend.common.base
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
 
-
+@Composable
 public expect fun rememberCustomFontFamily(): FontFamily
