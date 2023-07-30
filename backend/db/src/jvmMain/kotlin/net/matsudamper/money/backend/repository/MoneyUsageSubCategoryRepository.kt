db7f1cb42280126472cb1cb2c9bf6b2ef66e0b86
matsudamper <let@matsudamper.net>
2023-07-30 12:45:18 +0900

    所有していない時はエラーを返す

--- backend/db/src/jvmMain/kotlin/net/matsudamper/money/backend/repository/MoneyUsageSubCategoryRepository.kt
+++ backend/db/src/jvmMain/kotlin/net/matsudamper/money/backend/repository/MoneyUsageSubCategoryRepository.kt
@@ -71,10 +71,26 @@ class MoneyUsageSubCategoryRepository {
         categoryId: MoneyUsageCategoryId,
     ): GetSubCategoryResult {
         return runCatching {
+            @Suppress("FoldInitializerAndIfToElvis")
+            run {
+                val isOwner = DbConnection.use { connection ->
+                    DSL.using(connection)
+                        .select()
+                        .from(CATEGORIES)
+                        .where(
+                            DSL.value(true)
+                                .and(CATEGORIES.USER_ID.eq(userId.id))
+                                .and(CATEGORIES.MONEY_USAGE_CATEGORY_ID.eq(categoryId.id))
+                        )
+                        .fetchOne()
+                }
+
+                if (isOwner == null) {
+                    return GetSubCategoryResult.Failed(IllegalArgumentException("categoryId=$categoryId is Not owner."))
+                }
+            }
+
             DbConnection.use { connection ->
-
-                // TODO: 自分のものでない時はエラーにする
-
                 val records = DSL.using(connection)
                     .select(
                         SUB_CATEGORIES.USER_ID,
