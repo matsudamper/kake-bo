f1308d8173cdb7c3ddd070cee979dcda11b731c0
matsudamper <let@matsudamper.net>
2023-08-02 21:39:32 +0900

    ページングでリクエストが大量に送られる問題を修正

--- frontend/common/viewmodel/src/commonMain/kotlin/net/matsudamper/money/frontend/common/viewmodel/root/list/RootListViewModel.kt
+++ frontend/common/viewmodel/src/commonMain/kotlin/net/matsudamper/money/frontend/common/viewmodel/root/list/RootListViewModel.kt
@@ -4,15 +4,15 @@ import com.apollographql.apollo3.api.ApolloResponse
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.lastOrNull
-import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import net.matsudamper.money.frontend.common.ui.screen.RootListScreenUiState
@@ -48,33 +48,33 @@ public class RootListViewModel(
         ),
     ).also { uiStateFlow ->
         coroutineScope.launch {
-            viewModelStateFlow.collect { viewModelState ->
-                val results = flow {
-                    viewModelState.results
-                        .forEach { emitAll(it) }
-                }.toList()
+            viewModelStateFlow
+                .collectLatest { viewModelState ->
+                    combine(viewModelState.results) {
+                        it
+                    }.collect { results ->
+                        uiStateFlow.update { uiState ->
+                            uiState.copy(
+                                loadingState = RootListScreenUiState.LoadingState.Loaded(
+                                    loadToEnd = viewModelState.loadToEnd,
+                                    items = results.flatMap { it.data?.user?.moneyUsages?.nodes.orEmpty() }.map { result ->
+                                        RootListScreenUiState.Item(
+                                            title = result.title,
+                                            amount = result.amount.toString(),
+                                            date = result.date.toString(),
+                                            description = result.description,
+                                            category = run category@{
+                                                val subCategory = result.moneyUsageSubCategory ?: return@category null
 
-                uiStateFlow.update { uiState ->
-                    uiState.copy(
-                        loadingState = RootListScreenUiState.LoadingState.Loaded(
-                            loadToEnd = viewModelState.loadToEnd,
-                            items = results.flatMap { it.data?.user?.moneyUsages?.nodes.orEmpty() }.map {
-                                RootListScreenUiState.Item(
-                                    title = it.title,
-                                    amount = it.amount.toString(),
-                                    date = it.date.toString(),
-                                    description = it.description,
-                                    category = run category@{
-                                        val subCategory = it.moneyUsageSubCategory ?: return@category null
-
-                                        "${subCategory.name} / ${subCategory.category.name}"
+                                                "${subCategory.name} / ${subCategory.category.name}"
+                                            },
+                                        )
                                     },
                                 )
-                            },
-                        )
-                    )
+                            )
+                        }
+                    }
                 }
-            }
         }
     }.asStateFlow()
 
@@ -84,8 +84,8 @@ public class RootListViewModel(
 
     private fun fetch() {
         coroutineScope.launch {
-            if (viewModelStateFlow.value.isLoading) return@launch
             viewModelStateFlow.update { viewModelState ->
+                if (viewModelStateFlow.value.isLoading) return@launch
                 viewModelState.copy(
                     isLoading = true,
                 )
@@ -94,31 +94,34 @@ public class RootListViewModel(
             val cursor = if (beforeResults == null) {
                 null
             } else {
-                val tmp = beforeResults.lastOrNull()
+                val tmp = beforeResults.firstOrNull()
                 tmp?.data?.user?.moneyUsages?.cursor
                     ?: return@launch
             }
 
             val result = api.getHomeScreen(
                 cursor = cursor,
-            )
+            ).stateIn(coroutineScope, SharingStarted.Eagerly, null)
+                .filterNotNull()
+
             val loadToEnd = result.first().data?.user?.moneyUsages?.let { moneyUsages ->
                 moneyUsages.cursor == null
             }
-            if (loadToEnd == null) {
+            try {
+                if (loadToEnd != null) {
+                    viewModelStateFlow.update { viewModelState ->
+                        viewModelState.copy(
+                            results = viewModelState.results.plus(result),
+                            loadToEnd = loadToEnd,
+                        )
+                    }
+                }
+            } finally {
                 viewModelStateFlow.update { viewModelState ->
                     viewModelState.copy(
                         isLoading = false,
                     )
                 }
-                return@launch
-            }
-            viewModelStateFlow.update { viewModelState ->
-                viewModelState.copy(
-                    results = viewModelState.results.plus(result),
-                    isLoading = false,
-                    loadToEnd = loadToEnd,
-                )
             }
         }
     }
