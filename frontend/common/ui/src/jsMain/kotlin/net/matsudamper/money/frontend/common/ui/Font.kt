f2e8a7ab895b4ddb9b8a0d2d82932b180a731bc6
matsudamper <let@matsudamper.net>
2023-08-04 12:24:16 +0900

    format

--- frontend/common/ui/src/jsMain/kotlin/net/matsudamper/money/frontend/common/ui/Font.kt
+++ frontend/common/ui/src/jsMain/kotlin/net/matsudamper/money/frontend/common/ui/Font.kt
@@ -22,7 +22,10 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.js.Js
-import io.ktor.client.plugins.logging.*
+import io.ktor.client.plugins.logging.EMPTY
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
 import io.ktor.client.statement.readBytes
 import io.ktor.http.Url
