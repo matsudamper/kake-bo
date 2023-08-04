50ea12fa58c159e2dfdecc246dd07672ebaf3d23
matsudamper <let@matsudamper.net>
2023-08-04 13:14:46 +0900

    fix format

--- frontend/common/ui/src/commonMain/kotlin/net/matsudamper/money/frontend/common/ui/layout/Calendar.kt
+++ frontend/common/ui/src/commonMain/kotlin/net/matsudamper/money/frontend/common/ui/layout/Calendar.kt
@@ -3,7 +3,12 @@ package net.matsudamper.money.frontend.common.ui.layout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -14,14 +19,26 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.datetime.*
-import kotlinx.datetime.DayOfWeek.*
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
+import kotlinx.datetime.plus
+import kotlinx.datetime.todayIn
 
 @Composable
 public fun Calendar(
@@ -48,13 +65,13 @@ public fun Calendar(
 
     val calendarItems = remember(currentMonthDateList) {
         val offset = when (currentMonthDateList.first().dayOfWeek) {
-            SUNDAY -> 0
-            MONDAY -> 1
-            TUESDAY -> 2
-            WEDNESDAY -> 3
-            THURSDAY -> 4
-            FRIDAY -> 5
-            SATURDAY -> 6
+            DayOfWeek.SUNDAY -> 0
+            DayOfWeek.MONDAY -> 1
+            DayOfWeek.TUESDAY -> 2
+            DayOfWeek.WEDNESDAY -> 3
+            DayOfWeek.THURSDAY -> 4
+            DayOfWeek.FRIDAY -> 5
+            DayOfWeek.SATURDAY -> 6
         }
         (0 until offset).map { null } + currentMonthDateList
     }
