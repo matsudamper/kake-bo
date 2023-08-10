30e1b0f6b2078ba90b2f508556c29140171ad161
matsudamper <let@matsudamper.net>
2023-08-11 05:16:53 +0900

    情報を完全にクリアできていなかった問題を修正

--- frontend/common/schema/src/commonMain/kotlin/net/matsudamper/money/frontend/graphql/lib/ApolloPagingResponseCollector.kt
+++ frontend/common/schema/src/commonMain/kotlin/net/matsudamper/money/frontend/graphql/lib/ApolloPagingResponseCollector.kt
@@ -78,6 +78,9 @@ class ApolloPagingResponseCollector<D : Query.Data>(
         collectorFlow.update {
             listOf()
         }
+        _flow.update {
+            listOf()
+        }
     }
 
     companion object {
