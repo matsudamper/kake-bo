e2e461da1fe1cb73aba276dc0812cc651e1e2918
matsudamper <let@matsudamper.net>
2023-09-28 23:20:19 +0900

    最小幅を設定

--- frontend/common/ui/src/commonMain/kotlin/net/matsudamper/money/frontend/common/ui/screen/root/home/RootHomeTabPeriodScreen.kt
+++ frontend/common/ui/src/commonMain/kotlin/net/matsudamper/money/frontend/common/ui/screen/root/home/RootHomeTabPeriodScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
@@ -158,7 +159,7 @@ private fun BetweenLoaded(
         ) {
             var expanded by remember { mutableStateOf(false) }
             DropDownMenuButton(
-                modifier = Modifier,
+                modifier = Modifier.widthIn(min = 100.dp),
                 onClick = { expanded = !expanded },
             ) {
                 Text(uiState.categoryType)
