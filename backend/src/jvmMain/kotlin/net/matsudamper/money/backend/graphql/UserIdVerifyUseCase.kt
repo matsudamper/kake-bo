50f31ad0f5e1910ad03c2b8a93b6b77f2feb65ff
matsudamper <let@matsudamper.net>
2023-12-30 15:20:57 +0900

    format

--- backend/src/jvmMain/kotlin/net/matsudamper/money/backend/graphql/UserIdVerifyUseCase.kt
+++ backend/src/jvmMain/kotlin/net/matsudamper/money/backend/graphql/UserIdVerifyUseCase.kt
@@ -59,7 +59,6 @@ class UserIdVerifyUseCase(
         }
     }
 
-
     /**
      * @return isSuccess
      */
--- frontend/common/viewmodel/src/commonMain/kotlin/net/matsudamper/money/frontend/common/viewmodel/shared/FidoApi.kt
+++ frontend/common/viewmodel/src/commonMain/kotlin/net/matsudamper/money/frontend/common/viewmodel/shared/FidoApi.kt
@@ -2,8 +2,8 @@ package net.matsudamper.money.frontend.common.viewmodel.shared
 
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.ApolloResponse
-import net.matsudamper.money.frontend.graphql.LoginSettingScreenGetFidoInfoQuery
 import net.matsudamper.money.frontend.graphql.GraphqlClient
+import net.matsudamper.money.frontend.graphql.LoginSettingScreenGetFidoInfoQuery
 
 public class FidoApi(
     private val apolloClient: ApolloClient = GraphqlClient.apolloClient,
