2d11774b2791bb76cbf6da05b9274af92253298b
matsudamper <let@matsudamper.net>
2024-01-01 20:49:23 +0900

    redis用の環境変数を追加

--- backend/base/src/jvmMain/java/net/matsudamper/money/backend/base/ServerEnv.kt
+++ backend/base/src/jvmMain/java/net/matsudamper/money/backend/base/ServerEnv.kt
@@ -18,6 +18,7 @@ public object ServerEnv {
 
     public val userPasswordPepper: String = System.getenv("USER_PASSWORD_PEPPER")!!
 
-    public val challengeSecret: String = System.getenv("CHALLENGE_SECRET")!!
-        .takeIf { it.isNotEmpty() }!!
+    public val enableRedis: Boolean = System.getenv("ENABLE_REDIS")?.toBooleanStrictOrNull() ?: false
+    public val redisHost: String = System.getenv("REDIS_HOST")!!
+    public val redisPort: Int = System.getenv("REDIS_PORT")!!.toInt()
 }
