ac59ae22db959ca39eadbc95342a36414c8ad15f
matsudamper <let@matsudamper.net>
2024-01-01 22:33:17 +0900

    format

--- backend/datasource/db/src/jvmMain/resources/sql/2023-12-31.sql
+++ backend/datasource/db/src/jvmMain/resources/sql/2023-12-31.sql
@@ -1,13 +1,13 @@
 CREATE TABLE web_auth_authenticator
 (
     id                           INT PRIMARY KEY AUTO_INCREMENT,
-    user_id                      INT  NOT NULL,
-    name                         TEXT NOT NULL,
-    attestation_statement        TEXT NOT NULL,
-    attestation_statement_format TEXT NOT NULL,
-    attested_credential_data     TEXT NOT NULL,
+    user_id                      INT    NOT NULL,
+    name                         TEXT   NOT NULL,
+    attestation_statement        TEXT   NOT NULL,
+    attestation_statement_format TEXT   NOT NULL,
+    attested_credential_data     TEXT   NOT NULL,
     authenticator_extensions     TEXT,
-    counter                      BIGINT  NOT NULL,
+    counter                      BIGINT NOT NULL,
     created_at                   DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at                   DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT user_id_and_name UNIQUE (user_id, name)
