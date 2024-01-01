3996f5619f2fc979384ddc9a4a4e6797852e38e1
matsudamper <let@matsudamper.net>
2024-01-01 11:55:42 +0900

    snackbarが表示されない問題を修正

--- frontend/jsApp/src/jsMain/kotlin/Content.kt
+++ frontend/jsApp/src/jsMain/kotlin/Content.kt
@@ -1,5 +1,6 @@
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
@@ -10,6 +11,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveableStateHolder
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.IntSize
@@ -210,9 +212,14 @@ fun Content(
                 composeSizeProvider().value = it
             },
         snackbarHost = {
-            MySnackBarHost(
-                hostState = hostState,
-            )
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                MySnackBarHost(
+                    modifier = Modifier.align(Alignment.CenterEnd),
+                    hostState = hostState,
+                )
+            }
         },
     ) { paddingValues ->
         val tabHolder = rememberSaveableStateHolder()
