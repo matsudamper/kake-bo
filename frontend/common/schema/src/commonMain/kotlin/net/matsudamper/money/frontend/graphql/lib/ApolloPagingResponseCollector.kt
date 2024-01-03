f1d54fb21a41dd72b0ba02b0d7ccb4d4657d7012
matsudamper <let@matsudamper.net>
2024-01-04 02:59:20 +0900

    最初のデータしか取得できない問題を修正

--- frontend/common/schema/src/commonMain/kotlin/net/matsudamper/money/frontend/graphql/lib/ApolloPagingResponseCollector.kt
+++ frontend/common/schema/src/commonMain/kotlin/net/matsudamper/money/frontend/graphql/lib/ApolloPagingResponseCollector.kt
@@ -5,7 +5,9 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatten
 import kotlinx.coroutines.flow.flattenConcat
+import kotlinx.coroutines.flow.flattenMerge
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import com.apollographql.apollo3.ApolloClient
@@ -28,7 +30,7 @@ class ApolloPagingResponseCollector<D : Query.Data>(
         return collectorFlow
             .map { collectors ->
                 combine(collectors.map { it.flow }) { it.toList() }
-            }.flattenConcat()
+            }.flattenMerge()
     }
 
     val lastValue: List<ApolloResponseState<ApolloResponse<D>>> get() = collectorFlow.value.map { it.flow.value }
