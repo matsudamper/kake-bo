4ed0d45b785968fbe722a62dfa386651f7e4e27d
matsudamper <let@matsudamper.net>
2024-03-11 01:38:55 +0900

    API TokenのDBを追加

--- backend/datasource/db/src/jvmMain/resources/sql/2024-03-04.sql
+++ backend/datasource/db/src/jvmMain/resources/sql/2024-03-04.sql
@@ -10,3 +10,22 @@ ALTER TABLE user_password_extend_data
     MODIFY COLUMN update_datetime
         datetime default current_timestamp() not null
         ON UPDATE current_timestamp();
+
+CREATE TABLE api_tokens
+(
+    api_token_id     int auto_increment primary key       not null,
+    user_id          int                                  not null,
+    token_hash       varchar(512)                         not null,
+    salt             varchar(32)                          not null,
+    iteration_count  int                                  not null,
+    algorithm        varchar(255)                         not null,
+    key_length       int                                  not null,
+    permissions      TEXT                                 not null,
+    created_datetime datetime default current_timestamp() not null,
+    update_datetime  datetime default current_timestamp() not null ON UPDATE current_timestamp(),
+    constraint token
+        unique (token_hash),
+    index user_id (user_id)
+);
+
+DROP TABLE api_tokens
