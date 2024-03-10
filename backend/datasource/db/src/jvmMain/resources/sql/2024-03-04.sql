b808b7601c05d9db86b2ba1c1aba714b3fba88e8
matsudamper <let@matsudamper.net>
2024-03-11 01:38:24 +0900

    UPDATE DATETIMEが更新されていなかった問題を修正

--- backend/datasource/db/src/jvmMain/resources/sql/2024-03-04.sql
+++ backend/datasource/db/src/jvmMain/resources/sql/2024-03-04.sql
@@ -4,4 +4,9 @@ CREATE TABLE user_received_mails
     user_id               int                                  not null,
     created_datetime      datetime default current_timestamp() not null,
     raw_text              text                                 not null
-)
+);
+
+ALTER TABLE user_password_extend_data
+    MODIFY COLUMN update_datetime
+        datetime default current_timestamp() not null
+        ON UPDATE current_timestamp();
