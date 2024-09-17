0e1258793fc7a8c19c3a03bc2a35ec19a248d76f
matsudamper <let@matsudamper.net>
2024-09-18 02:37:17 +0900

    use java 17

--- frontend/androidApp/build.gradle.kts
+++ frontend/androidApp/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         targetSdk = 35
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
