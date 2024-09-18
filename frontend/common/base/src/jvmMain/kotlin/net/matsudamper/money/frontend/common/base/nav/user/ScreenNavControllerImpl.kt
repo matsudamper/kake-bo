49899f61361343bb042e4e437870deb432a05b9f
matsudamper <let@matsudamper.net>
2024-09-19 02:38:06 +0900

    navigationが動くようにする

--- frontend/common/base/src/jvmMain/kotlin/net/matsudamper/money/frontend/common/base/nav/user/ScreenNavControllerImpl.kt
+++ frontend/common/base/src/jvmMain/kotlin/net/matsudamper/money/frontend/common/base/nav/user/ScreenNavControllerImpl.kt
@@ -1,13 +1,16 @@
 package net.matsudamper.money.frontend.common.base.nav.user
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 @Immutable
 public actual class ScreenNavControllerImpl actual constructor(
     initial: ScreenStructure,
 ) : ScreenNavController<ScreenStructure> {
-    private var internalCurrentNavigation: ScreenStructure = initial
+    private var internalCurrentNavigation: ScreenStructure by mutableStateOf(initial)
     override val currentNavigation: ScreenStructure get() = internalCurrentNavigation
 
     override fun back() {
