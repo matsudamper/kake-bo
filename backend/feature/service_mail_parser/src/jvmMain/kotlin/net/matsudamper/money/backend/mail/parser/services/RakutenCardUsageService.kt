c47c2c0b0068ffecb196133d3fe89bc7d08aae6c
matsudamper <let@matsudamper.net>
2024-12-12 12:30:26 +0900

    楽天カードの確定版を追加

--- backend/feature/service_mail_parser/src/jvmMain/kotlin/net/matsudamper/money/backend/mail/parser/services/RakutenCardUsageService.kt
+++ backend/feature/service_mail_parser/src/jvmMain/kotlin/net/matsudamper/money/backend/mail/parser/services/RakutenCardUsageService.kt
@@ -24,17 +24,56 @@ internal object RakutenCardUsageService : MoneyUsageServices {
         }
         if (canHandle.any { it }.not()) return listOf()
 
+        return if (plain.contains("カード利用お知らせメール(確定版)")) {
+            parseConfirmedInfo(plain)
+        } else {
+            parseFastInfo(plain, subject)
+        }
+    }
+
+    private fun parseConfirmedInfo(plain: String): List<MoneyUsage> {
+        val lines = ParseUtil.splitByNewLine(plain)
+
+        val regex = """^(\d{4})/(\d{2})/(\d{2}) (.+?) (.+?) 円$""".toRegex()
+
+        return buildList {
+            for (line in lines) {
+                val result = regex.find(line)
+                val year = result?.groupValues?.getOrNull(1)?.toIntOrNull() ?: continue
+                val month = result.groupValues.getOrNull(2)?.toIntOrNull() ?: continue
+                val day = result.groupValues.getOrNull(3)?.toIntOrNull() ?: continue
+                val title = result.groupValues.getOrNull(4) ?: continue
+                val price = result.groupValues.getOrNull(5)
+                    ?.let { ParseUtil.getInt(it) } ?: continue
+
+                add(
+                    MoneyUsage(
+                        title = title,
+                        price = price,
+                        description = "",
+                        service = MoneyUsageServiceType.RakutenCard,
+                        dateTime = LocalDateTime.of(
+                            LocalDate.of(year, month, day),
+                            LocalTime.of(0, 0),
+                        ),
+                    ),
+                )
+            }
+        }
+    }
+
+    private fun parseFastInfo(plain: String, subject: String): List<MoneyUsage> {
         val lines = ParseUtil.splitByNewLine(plain)
         val priceTitleIndex = lines.indexOfFirst { it.startsWith("*ご利用金額*") }
         val priceLine = lines.getOrNull(priceTitleIndex + 1) ?: return listOf()
 
-        val regex = """^(\d{4})/(\d{2})/(\d{2}) (.+?) (.+?) 円$""".toRegex()
+        val result = """^(\d{4})/(\d{2})/(\d{2}) (.+?) (.+?) 円$""".toRegex()
             .find(priceLine)
 
-        val year = regex?.groupValues?.getOrNull(1)?.toIntOrNull() ?: return listOf()
-        val month = regex.groupValues.getOrNull(2)?.toIntOrNull() ?: return listOf()
-        val day = regex.groupValues.getOrNull(3)?.toIntOrNull() ?: return listOf()
-        val price = regex.groupValues.getOrNull(5)
+        val year = result?.groupValues?.getOrNull(1)?.toIntOrNull() ?: return listOf()
+        val month = result.groupValues.getOrNull(2)?.toIntOrNull() ?: return listOf()
+        val day = result.groupValues.getOrNull(3)?.toIntOrNull() ?: return listOf()
+        val price = result.groupValues.getOrNull(5)
             ?.let { ParseUtil.getInt(it) } ?: return listOf()
 
         return listOf(
