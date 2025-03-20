8d0dd0fa984baa06681d0b817e96be5b055fd80a
matsudamper <let@matsudamper.net>
2025-03-21 07:40:14 +0900

    format

--- backend/src/jvmMain/kotlin/net/matsudamper/money/backend/Main.kt
+++ backend/src/jvmMain/kotlin/net/matsudamper/money/backend/Main.kt
@@ -14,7 +14,6 @@ import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
-import io.ktor.server.http.content.static
 import io.ktor.server.http.content.staticFiles
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.calllogging.CallLogging
