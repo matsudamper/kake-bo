8ea8d7f4b8667252823488b81af2935adcc1599b
matsudamper <let@matsudamper.net>
2025-04-13 02:14:45 +0900

    こっちの方が確実に処理できそうなので修正

--- frontend/common/navigation/src/commonMain/kotlin/net/matsudamper/money/frontend/common/base/nav/NavHost.kt
+++ frontend/common/navigation/src/commonMain/kotlin/net/matsudamper/money/frontend/common/base/nav/NavHost.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import net.matsudamper.money.frontend.common.base.lib.rememberSaveableStateHolder
 import net.matsudamper.money.frontend.common.base.lifecycle.LocalScopedObjectStore
 import net.matsudamper.money.frontend.common.base.nav.user.IScreenStructure
@@ -21,17 +22,18 @@ public fun NavHost(
     val holder = rememberSaveableStateHolder("NavHostSaveableStateHolder")
     run {
         var beforeEntries: List<ScreenNavController.NavStackEntry> by remember { mutableStateOf(listOf()) }
-        LaunchedEffect(navController.backstackEntries) {
+        LaunchedEffect(navController) {
+            snapshotFlow { navController.backstackEntries }
+                .collect {
+                    val removedEntries = beforeEntries.filterNot { it in navController.backstackEntries }
+                        .filterNot { it.isHome }
+                        .filterNot { it.savedState }
+                    for (entry in removedEntries) {
+                        holder.removeState(entry.structure)
+                    }
 
-            val removedEntries = beforeEntries.filterNot { it in navController.backstackEntries }
-                .filterNot { it.isHome }
-                .filterNot { it.savedState }
-            for (entry in removedEntries) {
-                holder.removeState(entry.structure)
-            }
-
-            beforeEntries = navController.backstackEntries
-
+                    beforeEntries = navController.backstackEntries
+                }
         }
     }
     LaunchedEffect(navController.backstackEntries) {
