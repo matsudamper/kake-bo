be52f868ec37f304714d27542fe3aaefafa2ffd7
matsudamper <let@matsudamper.net>
2025-04-25 07:20:35 +0900

    iconの依存が無くなったのを修正

--- build-logic/src/main/kotlin/net/matsudamper/money/buildlogic/ComposePlugin.kt
+++ build-logic/src/main/kotlin/net/matsudamper/money/buildlogic/ComposePlugin.kt
@@ -2,8 +2,11 @@ package net.matsudamper.money.buildlogic
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.accessors.runtime.extensionOf
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.compose.ComposeExtension
+import org.jetbrains.compose.ComposePlugin
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 @Suppress("unused")
 class ComposePlugin : Plugin<Project> {
@@ -16,6 +19,14 @@ class ComposePlugin : Plugin<Project> {
             extensions.configure<ComposeExtension> {
 //                kotlinCompilerPlugin.set("1.5.2-beta01")
 //                kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=1.9.10")
+            }
+            configure<KotlinMultiplatformExtension> {
+                val compose = extensionOf(this, "compose") as ComposePlugin.Dependencies
+                sourceSets.getByName("commonMain") {
+                    dependencies {
+                        implementation(compose.materialIconsExtended)
+                    }
+                }
             }
         }
     }
