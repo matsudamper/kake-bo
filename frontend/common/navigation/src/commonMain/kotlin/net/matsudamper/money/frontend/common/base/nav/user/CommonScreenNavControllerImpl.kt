f4d43c91cff9623a59103cda2ae87f59dd9fcc5c
matsudamper <let@matsudamper.net>
2026-02-11 14:09:30 +0900

    何もしていないコードを削除

--- frontend/common/navigation/src/commonMain/kotlin/net/matsudamper/money/frontend/common/base/nav/user/CommonScreenNavControllerImpl.kt
+++ frontend/common/navigation/src/commonMain/kotlin/net/matsudamper/money/frontend/common/base/nav/user/CommonScreenNavControllerImpl.kt
@@ -15,7 +15,6 @@ internal class CommonScreenNavControllerImpl(
 
     override fun back() {
         val removed = navBackstack.removeAt(navBackstack.lastIndex)
-        navBackstack.reversed().first { it.stackGroupId == removed.stackGroupId }
         for (item in navBackstack.reversed()) {
             if (item.sameScreenId == removed.sameScreenId) {
                 navBackstack.removeAt(navBackstack.lastIndex)
