8603a340925c85109f702f3324609dd67e0e28f3
matsudamper <let@matsudamper.net>
2026-03-31 01:18:56 +0900

    ローディング表示を実装

--- frontend/common/ui/src/androidMain/kotlin/net/matsudamper/money/frontend/common/ui/layout/image/ZoomableImageDialog.android.kt
+++ frontend/common/ui/src/androidMain/kotlin/net/matsudamper/money/frontend/common/ui/layout/image/ZoomableImageDialog.android.kt
@@ -1,11 +1,15 @@
 package net.matsudamper.money.frontend.common.ui.layout.image
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,7 +18,8 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.window.DialogWindowProvider
-import coil3.compose.SubcomposeAsyncImage
+import coil3.compose.AsyncImagePainter
+import coil3.compose.rememberAsyncImagePainter
 import net.engawapg.lib.zoomable.rememberZoomState
 import net.engawapg.lib.zoomable.zoomable
 
@@ -46,23 +51,30 @@ public actual fun ZoomableImageDialog(
                 ),
             contentAlignment = Alignment.Center,
         ) {
-            SubcomposeAsyncImage(
-                model = imageUrl,
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
-                onSuccess = { successState ->
-                    zoomState.setContentSize(successState.painter.intrinsicSize)
-                },
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null,
-                        onClick = {},
+            val painter = rememberAsyncImagePainter(imageUrl)
+            when (val state = painter.state.collectAsState().value) {
+                is AsyncImagePainter.State.Empty,
+                is AsyncImagePainter.State.Error,
+                is AsyncImagePainter.State.Loading,
+                    -> {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator()
+                    }
+                }
+
+                is AsyncImagePainter.State.Success -> {
+                    LaunchedEffect(state.painter.intrinsicSize) {
+                        zoomState.setContentSize(state.painter.intrinsicSize)
+                    }
+                    Image(
+                        modifier = Modifier
+                            .zoomable(zoomState),
+                        painter = state.painter,
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
                     )
-                    .zoomable(zoomState),
-                loading = { ImageLoadingPlaceholder() },
-            )
+                }
+            }
         }
     }
 }
