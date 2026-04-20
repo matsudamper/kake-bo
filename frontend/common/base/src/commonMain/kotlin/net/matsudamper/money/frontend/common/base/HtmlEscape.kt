c4af5b636ac1eb2f2a5b217592e9aa747ed4e6af
matsudamper <let@matsudamper.net>
2026-04-21 02:04:47 +0900

    HTMLをエスケープする

---
+++ frontend/common/base/src/commonMain/kotlin/net/matsudamper/money/frontend/common/base/HtmlEscape.kt
@@ -0,0 +1,12 @@
+package net.matsudamper.money.frontend.common.base
+
+public object HtmlEscape {
+    public fun escape(text: String): String {
+        return text
+            .replace("&", "&amp;")
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("\"", "&quot;")
+            .replace("'", "&#39;")
+    }
+}
