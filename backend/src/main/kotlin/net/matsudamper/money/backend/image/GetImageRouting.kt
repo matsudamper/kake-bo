a03309b3f0cf50d805e6fde46d93930664cae71f
matsudamper <let@matsudamper.net>
2026-05-07 05:34:33 +0900

    エラーにする

--- backend/src/main/kotlin/net/matsudamper/money/backend/image/GetImageRouting.kt
+++ backend/src/main/kotlin/net/matsudamper/money/backend/image/GetImageRouting.kt
@@ -145,7 +145,7 @@ private suspend fun ApplicationCall.respondImageByDisplayId(
         relativePath = imageData.relativePath,
         displayId = displayId,
         userId = imageData.userId,
-        domain = ServerEnv.domain ?: "",
+        domain = ServerEnv.domain!!,
         purpose = purpose,
     )
     when (val result = gateway.read(readRequest)) {
