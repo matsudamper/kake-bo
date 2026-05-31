b5f4564bd047faeddf2cd504de25caa60db8dc51
matsudamper <let@matsudamper.net>
2026-06-01 01:49:48 +0900

    他と合わせる

--- backend/datasource/db/src/jvmMain/resources/sql/2026-03-30.sql
+++ backend/datasource/db/src/jvmMain/resources/sql/2026-03-30.sql
@@ -1,6 +1,5 @@
 CREATE TABLE user_timezone_setting
 (
     user_id                  INT NOT NULL PRIMARY KEY,
-    timezone_offset_minutes  INT NOT NULL DEFAULT 0,
-    CONSTRAINT fk_user_timezone_setting_user_id FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+    timezone_offset_minutes  INT NOT NULL DEFAULT 0
 );
